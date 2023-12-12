ec16baa7f2822dcda02f1b328789b3541b50180d
Phoboss <102321702+Phoboss33@users.noreply.github.com>
2023-12-12 23:52:52 +0500

    remake calc

--- main.js
+++ main.js
@@ -1,10 +1,20 @@
 let currentInput = '';
 let display = document.getElementById('display');
+let upperText = document.getElementById('upperText');
+
 let numberButtons = document.querySelectorAll('.number');
 let operationButtons = document.querySelectorAll('.operation');
 let clearButton = document.querySelector('.clear');
+let sqrtButton = document.querySelector('.sqrt');
 let calculateButton = document.querySelector('.calculate');
+let stepenButton = document.querySelector('.stepen');
 let isDot = false;
+
+let firstNumber = 0;
+let currentOperator;
+let vtoroeNumber = 0;
+
+let pressCount = 0;
 
 numberButtons.forEach(button => {
     button.addEventListener('click', () => {
@@ -14,38 +24,75 @@ numberButtons.forEach(button => {
 
 operationButtons.forEach(button => {
     button.addEventListener('click', () => {
-        pressKey(button.value);
+        pressOperator(button.value)
     });
 });
 
 clearButton.addEventListener('click', clearDisplay);
+sqrtButton.addEventListener('click', sqrt);
+stepenButton.addEventListener('click', stepen);
 calculateButton.addEventListener('click', calculateResult);
 
 function pressKey(key) {
-    if (key === '.' && !isDot) {
-        isDot = true;
+    if (key === "." && display.value.includes(".")) {
         return;
     }
-    if (key === '.' && (currentInput === '')) {
-        currentInput += '0';
+    if (display.value === "" && key === ".") {
+        display.value += "0";
     }
-    currentInput += key;
-    display.value = currentInput;
+    currentInput = key;
+    display.value += currentInput;
 }
 
 function clearDisplay() {
-    currentInput = '';
+    currentInput = "";
     display.value = currentInput;
 }
 
-function calculateResult() {
-    try {
-        currentInput = currentInput.replace(/%/g, '%').replace(/\^/g, '**');
-
-        currentInput = eval(currentInput).toString();
-        display.value = currentInput;
-    } catch (error) {
-        alert('Ошибка в выражении: ' + error);
-        clearDisplay();
+function pressOperator(key) {
+    if (display.value != "") {
+        pressCount++;
+        currentOperator = key;
+        upperText.textContent = firstNumber + currentOperator;
+        if (pressCount % 2 == 0) {
+            vtoroeNumber = display.value;
+            pressCount = 0;
+            display.value = eval(firstNumber + currentOperator + vtoroeNumber);
+        }
+        else {
+            firstNumber = display.value;
+            display.value = "";
+            upperText.textContent = firstNumber + currentOperator;
+        }
     }
+    console.log(vtoroeNumber);
+    console.log(pressCount);
+}
+
+function calculateResult() {
+    upperText.textContent = ""
+    vtoroeNumber = display.value;
+    display.value = eval(firstNumber + currentOperator + vtoroeNumber);
+    firstNumber = 0;
+    vtoroeNumber = 0;
+    currentOperator = "";
+    pressCount = 0;
+}
+
+function clearDisplay() {
+    upperText.textContent = ""
+    display.value = "";
+    firstNumber = 0;
+    vtoroeNumber = 0;
+    currentOperator = "";
+    pressCount = 0;
+}
+
+function stepen() {
+    display.value = Math.pow(display.value, 2)
+
+}
+
+function sqrt() {
+    display.value = Math.sqrt(display.value)
 }
